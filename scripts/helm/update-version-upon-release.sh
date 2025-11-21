9661bf82a6ec1eb346a30f1d309f26aafb42b459
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2025-11-21 04:28:17 +0000

    ci: add rawfile to branch prep, fix update script and staging, update helm docs
    Signed-off-by: Abhinandan Purkait <purkaitabhinandan@gmail.com>

--- scripts/helm/update-version-upon-release.sh
+++ scripts/helm/update-version-upon-release.sh
@@ -5,16 +5,30 @@ help() {
 Usage: $(basename "$0") [OPTIONS]
 
 Options:
-  -h, --help   Show this help message.
+  -h, --help       Show this help message.
+  --tag <version>  Set chart and app version to <version> (remove leading 'v' if present).
 
-This script removes the "-prerelease" suffix from chart & dependencies versions in Chart.yaml.
+This script removes the "-prerelease" suffix from chart & dependencies versions in Chart.yaml,
+or sets the version/appVersion to the supplied tag.
 EOF
 }
 
 update_chart_yaml() {
-  yq_ibl '.version |= sub("-prerelease$"; "") | .appVersion |= sub("-prerelease$"; "")' "$CHART_YAML"
-  yq_ibl '(.dependencies[] | select(.name == "openebs-crds") | .version) |= sub("-prerelease$"; "")' "$CHART_YAML"
-  yq_ibl '.version |= sub("-prerelease$"; "")' "$CRD_CHART_YAML"
+  if [ -n "$TAG" ]; then
+    # Remove leading 'v' from tag if present
+    VERSION="${TAG#v}"
+    # Set chart version & appVersion to VERSION
+    yq_ibl ".version = \"$VERSION\" | .appVersion = \"$VERSION\"" "$CHART_YAML"
+    yq_ibl "(.dependencies[] | select(.name == \"openebs-crds\") | .version) = \"$VERSION\"" "$CHART_YAML"
+    yq_ibl ".version = \"$VERSION\"" "$CRD_CHART_YAML"
+  else
+    # Existing logic: remove '-prerelease' suffix
+    yq_ibl '.version |= sub("-prerelease$"; "") | .appVersion |= sub("-prerelease$"; "")' "$CHART_YAML"
+    yq_ibl '(.dependencies[] | select(.name == "openebs-crds") | .version) |= sub("-prerelease$"; "")' "$CHART_YAML"
+    yq_ibl '.version |= sub("-prerelease$"; "")' "$CRD_CHART_YAML"
+  fi
+
+  # The rest of the existing dependencies updates
   yq_ibl '(.dependencies[] | select(.name == "localpv-provisioner") | .version) |= sub("-prerelease$"; "")' "$CHART_YAML"
   yq_ibl '(.dependencies[] | select(.name == "zfs-localpv") | .version) |= sub("-prerelease$"; "")' "$CHART_YAML"
   yq_ibl '(.dependencies[] | select(.name == "lvm-localpv") | .version) |= sub("-prerelease$"; "")' "$CHART_YAML"
@@ -33,6 +47,8 @@ CRD_CHART_YAML="$CHART_DIR/charts/$CRD_CHART_NAME/Chart.yaml"
 source "$ROOT_DIR/mayastor/scripts/utils/yaml.sh"
 source "$ROOT_DIR/mayastor/scripts/utils/log.sh"
 
+TAG=""
+
 # Parse arguments
 while [ "$#" -gt 0 ]; do
   case $1 in
@@ -40,11 +56,16 @@ while [ "$#" -gt 0 ]; do
       help
       exit 0
       ;;
+    --tag)
+      shift
+      TAG="$1"
+      ;;
     *)
       help
       die "Unknown option: $1"
       ;;
   esac
+  shift
 done
 
 update_chart_yaml
